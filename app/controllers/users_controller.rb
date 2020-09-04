cf014aca4268bd64891edc657b566e95191cfd3a
alexoid <alex_zam@hotmail.es>
2020-09-04 17:26:47 -0500

    add index method and link to user new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    def index
+      @users=User.all
+    end  
     def new
         @user = User.new
     end
@@ -8,11 +11,14 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
        
         if @user.save
-          redirect_to new_user_path
+          redirect_to users_url
         else
           render :new
         end     
     end
+    def edit
+      @user = User.find(params[:id])
+    end  
 
     private
     def user_params        
