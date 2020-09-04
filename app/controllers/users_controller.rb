89bef9d91a6d8daa7064ad5926882d97fe109c5f
JasemDuncan <jasemvalencia@gmail.com>
2020-09-04 14:38:21 -0500

    Design Rails Form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,10 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
-    def create
-            
+    def create 
+      puts "asdsadsadadasdasdsadsddddd"
         @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        # @user = User.new(user_params)
        
         if @user.save
           redirect_to new_user_path
@@ -13,10 +14,9 @@ class UsersController < ApplicationController
         end     
     end
 
-    # # private
-    # # def user_params        
-    # #     params.require(:user).permit(:username, :email, :password)
-    # # end
-    
+    private
+    def user_params        
+        params.require(:user).permit(:username, :email, :password)
+    end   
 
 end
