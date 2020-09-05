01f8b18e1d2cbe6b95b293a45e66b9de41dc13b3
alexoid <alex_zam@hotmail.es>
2020-09-04 23:57:26 -0500

    add edit method show method and patch form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
     def index
       @users=User.all
-    end  
+    end 
+
     def new
         @user = User.new
     end
+    def show
+      @user = User.find(params[:id])
+    end   
     def create 
       puts "asdsadsadadasdasdsadsddddd"
         #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
