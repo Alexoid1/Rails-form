1b88103d97d8cc809d8eed0d9710a798ede2c29a
alexoid <alex_zam@hotmail.es>
2020-09-06 11:26:44 -0500

    add update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,16 @@ class UsersController < ApplicationController
     end
     def edit
       @user = User.find(params[:id])
-    end  
+    end
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to users_url
+      else
+        
+        render :edit
+      end    
+    end    
 
     private
     def user_params        
